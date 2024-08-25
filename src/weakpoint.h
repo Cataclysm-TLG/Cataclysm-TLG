323685e99e70af66e945c1ba563ec3cff7ce0430
GuardianDll <antisim009@gmail.com>
2024-08-25 15:23:13 +0200

    make weakpoints able to evaluate conditions

--- src/weakpoint.h
+++ src/weakpoint.h
@@ -10,6 +10,7 @@
 #include <utility>
 #include <vector>
 
+#include "condition.h"
 #include "damage.h"
 #include "translation.h"
 #include "type_id.h"
@@ -134,6 +135,11 @@ struct weakpoint {
     std::unordered_map<damage_type_id, float> crit_mult;
     // A list of required effects.
     std::vector<efftype_id> required_effects;
+    // A list of effects that will disable this weakpoint.
+    std::vector<efftype_id> disabled_by;
+    // Dialogue conditions of weakpoint
+    std::function<bool( dialogue & )> condition;
+    bool has_condition = false;
     // A list of effects that may trigger by hitting this weak point.
     std::vector<weakpoint_effect> effects;
     // Constant coverage multipliers, depending on the attack type.
