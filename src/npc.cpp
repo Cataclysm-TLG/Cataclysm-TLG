4305eabd3c30c7a03cd2e212ed828e5b6bb0a1fe
mqrause <38702195+mqrause@users.noreply.github.com>
2024-09-26 00:59:03 +0200

    use typed tripoints in npcmove and adjacent code (#76623)
    Co-authored-by: mqrause <martin@qrause.de>

--- src/npc.cpp
+++ src/npc.cpp
@@ -230,7 +230,7 @@ npc::npc()
     last_updated = calendar::turn;
     last_player_seen_pos = std::nullopt;
     last_seen_player_turn = 999;
-    wanted_item_pos = tripoint_min;
+    wanted_item_pos = tripoint_bub_ms_min;
     guard_pos = std::nullopt;
     goal = tripoint_abs_omt( tripoint_min );
     fetching_item = false;
@@ -2731,9 +2731,9 @@ std::string npc::opinion_text() const
     return ret;
 }
 
-static void maybe_shift( tripoint &pos, const point &d )
+static void maybe_shift( tripoint_bub_ms &pos, const point &d )
 {
-    if( pos != tripoint_min ) {
+    if( pos != tripoint_bub_ms_min ) {
         pos += d;
     }
 }
@@ -2762,7 +2762,7 @@ void npc::reboot()
     path.clear();
     last_player_seen_pos = std::nullopt;
     last_seen_player_turn = 999;
-    wanted_item_pos = tripoint_min;
+    wanted_item_pos = tripoint_bub_ms_min;
     guard_pos = std::nullopt;
     goal = no_goal_point;
     fetching_item = false;
@@ -3333,7 +3333,7 @@ std::function<bool( const tripoint & )> npc::get_path_avoid() const
               here.move_cost( p ) > 2 ) ) {
             return true;
         }
-        if( sees_dangerous_field( p ) ) {
+        if( sees_dangerous_field( tripoint_bub_ms( p ) ) ) {
             return true;
         }
         if( !can_move_to_vehicle_tile( here.getglobal( p ) ) ) {
