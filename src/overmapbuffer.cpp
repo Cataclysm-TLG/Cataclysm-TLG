f30c05fc2a77b70ff339f1a5c9e12c564d1b0b2d
Blueflowers <Canniverrumble@protonmail.com>
2024-09-29 11:34:36 +0200

    typing 'default' into the world_test EOC returns you to main world
    update EOC to mention the 'default' option
    
    changed 'map_prefix' to 'world_prefix' where appropriate. Began some work on making monsters work while swapping
    
    moved 'world_prefix' to 'map.h'
    
    made overmap save it's data to /worlds subdirectory if world_prefix is set
    
    monsters are now preserved between worlds
    
    added map memory to things that are saved
    
    add a check to stop teleports in other worlds from causing a world swap
    
    add mention of the EOC change in EFFECT_ON_CONDITION.md
    
    hacky way of preventing toilet related mapgen
    
    add a seperate bool variable from 'new_game', 'swapping_worlds'. Also added a check to prevent revification when swapping worlds, as that might cause crashes.
    
    savefile now stores the world_prefix the player was on
    
    moved world swap functions over to new 'multiworld.h/cpp' files and fixed issues with the maps not being properly saved when swapping
    
    don't teleport player to saved in world before the game has even loaded
    
    add 'u_in_world' and add an example usage to 'EOC_world_swap'
    
    made u_teleport display a succesful message if either the teleport or the world swap succeded
    
    Delete sta1xwkz
    
    Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    bump savegame version number on test
    
    apply clang tidy suggestions
    
    applied more clang-tidy suggestions
    
    Apply suggestions from code review
    
    Co-authored-by: Brambor <tondadrd@seznam.cz>
    
    remove pointless comment
    
    Update multiworld.cpp
    
    Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    add code optimization
    
    remove redundant declarations
    
    Update multiworld.h

--- src/overmapbuffer.cpp
+++ src/overmapbuffer.cpp
@@ -87,7 +87,14 @@ int camp_reference::get_distance_from_bounds() const
 
 std::string overmapbuffer::terrain_filename( const point_abs_om &p )
 {
-    return string_format( "o.%d.%d", p.x(), p.y() );
+    std::string dimension_prefix = g->get_dimension_prefix();
+    if( !dimension_prefix.empty() ) {
+        return PATH_INFO::world_base_save_path() / "dimensions" / dimension_prefix /
+               string_format( "o.%d.%d",
+                              p.x(), p.y() );
+    } else {
+        return PATH_INFO::world_base_save_path() / string_format( "o.%d.%d", p.x(), p.y() );
+    }
 }
 
 cata_path overmapbuffer::player_filename( const point_abs_om &p )
@@ -338,23 +345,9 @@ overmap *overmapbuffer::get_existing( const point_abs_om &p )
         // checked in a previous call of this function).
         return nullptr;
     }
-
-    cata_path path;
-
-    const std::string terfilename = overmapbuffer::terrain_filename( p );
-    const std::filesystem::path terfilename_path = std::filesystem::u8path( terfilename );
-
-    if( world_generator->active_world->has_compression_enabled() ) {
-        assure_dir_exist( PATH_INFO::world_base_save_path() / zzip_overmap_directory );
-        path = PATH_INFO::world_base_save_path() / zzip_overmap_directory / terfilename_path
-               +
-               zzip_suffix;
-    } else {
-        assure_dir_exist( PATH_INFO::world_base_save_path() );
-        path = PATH_INFO::world_base_save_path() / terfilename_path;
-    }
-
-    if( file_exist( path ) ) {
+    assure_dir_exist( PATH_INFO::world_base_save_path() / "dimensions" /
+                      g->get_dimension_prefix() );
+    if( file_exist( PATH_INFO::world_base_save_path() / terrain_filename( p ) ) ) {
         // File exists, load it normally (the get function
         // indirectly call overmap::open to do so).
         return &get( p );
