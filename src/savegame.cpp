fd1b0c0c7dc2873f499be794edca5cde09654872
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-10-19 12:41:46 +0100

    It helps if you add save/load handling

--- src/savegame.cpp
+++ src/savegame.cpp
@@ -512,6 +512,8 @@ void overmap::unserialize( const JsonObject &jsobj )
                 }
                 cities.push_back( new_city );
             }
+        } else if( name == "city_tiles" ) {
+            om_member.read( city_tiles );
         } else if( name == "connections_out" ) {
             om_member.read( connections_out );
         } else if( name == "roads_out" ) {
@@ -1248,6 +1250,9 @@ void overmap::serialize( std::ostream &fout ) const
         json.end_object();
     }
     json.end_array();
+    fout << std::endl;
+
+    json.member( "city_tiles", city_tiles );
     fout << std::endl;
 
     json.member( "connections_out", connections_out );
