06ddc92919f8de9affb0b28fef5ed9f53c6eadd3
typeryougishiki <typeryougishiki@163.com>
2025-02-28 23:50:02 +0800

    Add “allow_nested” option and function annotations to item::best_pocket and item_contents::best_pocket.

--- src/item_contents.h
+++ src/item_contents.h
@@ -37,13 +37,21 @@ class item_contents
         explicit item_contents( const std::vector<pocket_data> &pockets );
 
         /**
-          * returns an item_location and pointer to the best pocket that can contain the item @it
-          * checks all items contained in every pocket
-          * only checks CONTAINER pocket type
-          */
+         * Return an item_location and a pointer to the best pocket that can contain the item @it.
+         * if param allow_nested=true Check all items contained in every pocket of CONTAINER pocket type,
+         * otherwise, only check this item contents' pockets.
+         * @param it the item that function will find the best pocket that can contain it
+         * @param this_loc location of it
+         * @param avoid item that will be avoided in recursive lookup item pocket
+         * @param allow_sealed allow use sealed pocket
+         * @param ignore_settings ignore pocket setting
+         * @param nested whether the current call is nested (used recursively).
+         * @param ignore_rigidity ignore pocket rigid
+         * @param allow_nested whether nested pockets should be checked
+         */
         std::pair<item_location, item_pocket *> best_pocket( const item &it, item_location &this_loc,
                 const item *avoid = nullptr, bool allow_sealed = false, bool ignore_settings = false,
-                bool nested = false, bool ignore_rigidity = false );
+                bool nested = false, bool ignore_rigidity = false, bool allow_nested = true );
 
         units::length max_containable_length( bool unrestricted_pockets_only = false ) const;
         units::length min_containable_length() const;
