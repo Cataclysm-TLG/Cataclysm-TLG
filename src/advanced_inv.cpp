7d35d30d7dfa0d982a98fa50e7e98ed7fe393041
marilynias <98028585+marilynias@users.noreply.github.com>
2025-03-25 03:21:26 +0100

    fix both AIM-panels selected (#80303)
    * fix both panels selected
    
    * add tests
    
    * Update advanced_inv.h
    
    * Update advanced_inventory_test.cpp

--- src/advanced_inv.cpp
+++ src/advanced_inv.cpp
@@ -1848,8 +1848,6 @@ bool advanced_inventory::action_unload( advanced_inv_listitem *sitem,
 
 void advanced_inventory::process_action( const std::string &input_action )
 {
-    dest = src == advanced_inventory::side::left ? advanced_inventory::side::right :
-           advanced_inventory::side::left;
     recalc = false;
     // source and destination pane
     advanced_inventory_pane &spane = panes[src];
@@ -2009,6 +2007,8 @@ void advanced_inventory::process_action( const std::string &input_action )
             popup_getkey( _( "There's no vehicle storage space there." ) );
         }
     }
+    dest = src == advanced_inventory::side::left ? advanced_inventory::side::right :
+           advanced_inventory::side::left;
 }
 
 void advanced_inventory::display()
@@ -2097,7 +2097,6 @@ void advanced_inventory::display()
             do_return_entry();
             return;
         }
-
 
         if( ui ) {
             ui->invalidate_ui();
