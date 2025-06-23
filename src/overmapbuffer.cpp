5d81ecc30d44cb1226c043d32dfde605eec27679
Procyonae <45432782+Procyonae@users.noreply.github.com>
2025-06-23 03:29:11 +0100

    Adds a vertical omt stack scope for mapgen parameters (#81296)
    * Add smallmap scope
    
    * Make the map private, use mapgen_arguments over raw name value pairs, add serialisation
    
    * smallmap -> omt_stack
    
    Only get md.pos().xy() once
    Appease Patrik
    
    * tripoint_abs_omt::zero -> tripoint_abs_omt::invalid
    
    * Fix storing all scopes of parameters
    
    Which has somehow borked everything more, now the game won't load
    
    * Unexplode the game
    
    * Add displaying omt_stack parameters to debug overmap_editor
    
    * Appease our clang overlords

--- src/overmapbuffer.cpp
+++ src/overmapbuffer.cpp
@@ -29,6 +29,7 @@
 #include "game_constants.h"
 #include "line.h"
 #include "map.h"
+#include "mapgendata.h"
 #include "memory_fast.h"
 #include "mod_manager.h"
 #include "mongroup.h"
@@ -1092,6 +1093,13 @@ std::optional<overmap_special_id> overmapbuffer::overmap_special_at(
 {
     const overmap_with_local_coords om_loc = get_om_global( loc );
     return om_loc.om->overmap_special_at( om_loc.local );
+}
+
+std::optional<mapgen_arguments> overmapbuffer::get_existing_omt_stack_arguments(
+    const point_abs_omt &p )
+{
+    const overmap_with_local_coords om_loc = get_om_global( p );
+    return om_loc.om->get_existing_omt_stack_arguments( p );
 }
 
 bool overmapbuffer::check_ot( const std::string &type, ot_match_type match_type,
