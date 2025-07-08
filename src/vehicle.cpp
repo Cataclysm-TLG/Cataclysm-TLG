907a5f8b5a070833b5b8616c80b7ac0ab8836b3c
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2025-07-08 13:20:16 -0700

    Replace shock absorbers with driver skill

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -123,6 +123,7 @@ static const proficiency_id proficiency_prof_athlete_master( "prof_athlete_maste
 static const proficiency_id proficiency_prof_boat_pilot( "prof_boat_pilot" );
 static const proficiency_id proficiency_prof_driver( "prof_driver" );
 
+static const skill_id skill_driving( "driving" );
 static const skill_id skill_swimming( "swimming" );
 
 static const vpart_id vpart_power_cord( "power_cord" );
@@ -7594,14 +7595,14 @@ void vehicle::damage_all( int dmg1, int dmg2, const damage_type_id &type,
 
     for( const vpart_reference &vpr : get_all_parts() ) {
         vehicle_part &vp = vpr.part();
-        const vpart_info &vpi = vp.info();
-        const int distance = 1 + trig_dist( vp.mount, impact );
+        const int distance = 1 + square_dist( vp.mount, impact );
+        int reduced = 0;
+        // Driver skill can mitigate damage.
+        if( get_driver() && get_driver()->get_skill_level( skill_driving ) >= rng( 0, 11 ) ) {
+            reduced = rng( 0, 1 );
+        }
         if( distance > 1 ) {
-            int net_dmg = rng( dmg1, dmg2 ) / 1 + ( distance * distance );
-            if( vpi.location != part_location_structure || !vpi.has_flag( "PROTRUSION" ) ) {
-                int randomize = rng( 0, net_dmg / 2 );
-                net_dmg -= randomize;
-            }
+            int net_dmg = rng( dmg1, dmg2 ) / ( reduced + distance * distance );
             damage_direct( get_map(), vp, net_dmg, type );
         }
     }
