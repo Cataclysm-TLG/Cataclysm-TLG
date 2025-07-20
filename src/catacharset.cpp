0d98d561b8622b56fcc6604af5dc6c569123800b
worm-girl <94415528+worm-girl@users.noreply.github.com>
2025-07-20 13:38:30 -0700

    more build fixes

--- src/catacharset.cpp
+++ src/catacharset.cpp
@@ -361,7 +361,7 @@ inline std::wstring utf8_to_wstr( const std::string &utf8 )
 
 #else
     iconv_t cd = iconv_open( "UTF-32LE", "UTF-8" );
-    if( cd == static_cast<iconv_t>( -1 ) ) {
+    if( cd == reinterpret_cast<iconv_t>( -1 ) ) {
         throw std::runtime_error( "iconv_open failed in utf8_to_wstr" );
     }
 
@@ -377,7 +377,7 @@ inline std::wstring utf8_to_wstr( const std::string &utf8 )
     size_t res = iconv( cd, &inbuf, &in_bytes_left, &outptr, &out_bytes_left );
     iconv_close( cd );
 
-    if( res == static_cast<size_t>( -1 ) ) {
+    if( res == reinterpret_cast<size_t>( -1 ) ) {
         throw std::runtime_error( std::string( "iconv failed in utf8_to_wstr: " ) + strerror( errno ) );
     }
 
@@ -401,7 +401,7 @@ inline std::string wstr_to_utf8( const std::wstring &wstr )
 
 #else
     iconv_t cd = iconv_open( "UTF-8", "UTF-32LE" );
-    if( cd == static_cast<iconv_t>( -1 ) ) {
+    if( cd == reinterpret_cast<iconv_t>( -1 ) ) {
         throw std::runtime_error( "iconv_open failed in wstr_to_utf8" );
     }
 
@@ -417,7 +417,7 @@ inline std::string wstr_to_utf8( const std::wstring &wstr )
     size_t res = iconv( cd, &inbuf, &in_bytes_left, &outptr, &out_bytes_left );
     iconv_close( cd );
 
-    if( res == static_cast<size_t>( -1 ) ) {
+    if( res == reinterpret_cast<size_t>( -1 ) ) {
         throw std::runtime_error( std::string( "iconv failed in wstr_to_utf8: " ) + strerror( errno ) );
     }
 
