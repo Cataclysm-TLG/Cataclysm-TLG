akrieger/Cataclysm-DDA

move volume and weight vals to math

src/talker_character.cpp
@@ -1009,6 +1009,11 @@ int talker_character_const::get_weight() const
     return units::to_milligram( me_chr_const->get_weight() );
 }
 
+int talker_character_const::get_volume() const
+{
+    return units::to_milliliter( me_chr_const->get_total_volume() );
+}
+
 void talker_character::set_height( int amount )
 {
     me_chr->set_base_height( amount );

Cataclysm-TLG/Cataclysm-TLG

src/talker_character.cpp
@@ -999,6 +999,11 @@ int talker_character_const::get_weight() const
     return units::to_milligram( me_chr_const->get_weight() );
 }
 
+int talker_character_const::get_volume() const
+{
+    return units::to_milliliter( me_chr_const->get_total_character_volume() );
+}
+
 void talker_character::set_height( int amount )
 {
     me_chr->set_base_height( amount );