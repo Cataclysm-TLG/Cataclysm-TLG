CleverRaven/Cataclysm-DDA

casting a spell casts the wrong spell (#76137)
* Fix an issue where trying to cast a spell causes
the wrong spell to be cast
currently selecting a spell in a long sorted spell
list will cast the wrong spell due to select spell returing a sorted
spell id while handle_action.cpp looks only at the unsorted spells

fixes #76042
Fix an issue where trying to cast a spell causes
the wrong spell to be cast

* Apply suggestions from automated code review

Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

* solve issue with closing spell menu with no spell
selected
streamline the feature and fix a bug

* removed an unwanted code comment from
the previous commit

* Apply suggestions from automated code review

local astyle somehow missed those

Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---------

Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

src/magic.cpp
@@ -2840,7 +2840,7 @@ int known_magic::get_invlet( const spell_id &sp, std::set<int> &used_invlets )
     return 0;
 }
 
-int known_magic::select_spell( Character &guy )
+spell &known_magic::select_spell( Character &guy )
 {
     std::vector<spell *> known_spells_sorted = get_spells();
 
@@ -2933,8 +2933,12 @@ int known_magic::select_spell( Character &guy )
     spell_menu.query( true, 50, true );
 
     casting_ignore = static_cast<spellcasting_callback *>( spell_menu.callback )->casting_ignore;
-
-    return spell_menu.ret;
+    if( spell_menu.ret < 0 ) {
+        static spell null_spell_reference( spell_id::NULL_ID() );
+        return null_spell_reference;
+    }
+    spell *selected_spell = known_spells_sorted[spell_menu.ret];
+    return *selected_spell;
 }
 
 void known_magic::on_mutation_gain( const trait_id &mid, Character &guy )
@@ -2991,7 +2995,6 @@ static std::string color_number( const float num )
         return colorize( "0", c_white );
     }
 }
-
 static void draw_spellbook_info( const spell_type &sp )
 {
     const spell fake_spell( sp.id );

Cataclysm-TLG/Cataclysm-TLG

src/magic.cpp
@@ -2972,7 +2972,7 @@ int known_magic::get_invlet( const spell_id &sp, std::set<int> &used_invlets )
     return 0;
 }
 
-int known_magic::select_spell( Character &guy )
+spell &known_magic::select_spell( Character &guy )
 {
     // max width of spell names
     const int max_spell_name_length = get_spellname_max_width();
@@ -3048,8 +3048,12 @@ int known_magic::select_spell( Character &guy )
     spell_menu.query( true, -1, true );
 
     casting_ignore = static_cast<spellcasting_callback *>( spell_menu.callback )->casting_ignore;
-
-    return spell_menu.ret;
+    if( spell_menu.ret < 0 ) {
+        static spell null_spell_reference( spell_id::NULL_ID() );
+        return null_spell_reference;
+    }
+    spell *selected_spell = known_spells[spell_menu.ret];
+    return *selected_spell;
 }
 
 void known_magic::on_mutation_gain( const trait_id &mid, Character &guy )