c77d38eb2c8f289eedc56f2737518b78f7b95a6e
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2025-09-16 00:10:23 -0700

    Modernize gastropod tongue attack + venom

--- src/mutation.cpp
+++ src/mutation.cpp
@@ -77,7 +77,6 @@ static const trait_id trait_BURROW( "BURROW" );
 static const trait_id trait_BURROWLARGE( "BURROWLARGE" );
 static const trait_id trait_CHAOTIC_BAD( "CHAOTIC_BAD" );
 static const trait_id trait_ECHOLOCATION( "ECHOLOCATION" );
-static const trait_id trait_GASTROPOD_EXTREMITY2( "GASTROPOD_EXTREMITY2" );
 static const trait_id trait_GASTROPOD_EXTREMITY3( "GASTROPOD_EXTREMITY3" );
 static const trait_id trait_GLASSJAW( "GLASSJAW" );
 static const trait_id trait_LONG_TONGUE2( "LONG_TONGUE2" );
@@ -877,7 +876,6 @@ void Character::activate_cached_mutation( const trait_id &mut )
         get_map().add_field( pos_bub(), fd_web, 1 );
         add_msg_if_player( _( "You start spinning web with your spinnerets!" ) );
     } else if( mut == trait_LONG_TONGUE2 ||
-               mut == trait_GASTROPOD_EXTREMITY2 ||
                mut == trait_GASTROPOD_EXTREMITY3 ) {
         tdata.powered = false;
         assign_activity( ACT_PULL_CREATURE, to_moves<int>( 1_seconds ), 0, 0, mutation_name( mut ) );
