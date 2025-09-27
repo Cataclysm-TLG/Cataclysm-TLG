akrieger/Cataclysm-DDA

use MAX_VIEW_DISTANCE in place of hardcoded values. unmagic air opacity

src/activity_actor.cpp
@@ -818,8 +818,8 @@ void hacking_activity_actor::finish( player_activity &act, Character &who )
             // currently all things that can be hacked have equivalent alarm failure states.
             // this may not always be the case with new hackable things.
             get_event_bus().send<event_type::triggers_alarm>( who.getID() );
-            sounds::sound( who.pos(), 60, sounds::sound_t::music, _( "an alarm sound!" ), true, "environment",
-                           "alarm" );
+            sounds::sound( who.pos(), MAX_VIEW_DISTANCE, sounds::sound_t::music, _( "an alarm sound!" ), true,
+                           "environment", "alarm" );
             break;
         case hack_result::NOTHING:
             who.add_msg_if_player( _( "You fail the hack, but no alarms are triggered." ) );

Cataclysm-TLG/Cataclysm-TLG

src/activity_actor.cpp
@@ -914,7 +914,7 @@ void hacking_activity_actor::finish( player_activity &act, Character &who )
             // currently all things that can be hacked have equivalent alarm failure states.
             // this may not always be the case with new hackable things.
             get_event_bus().send<event_type::triggers_alarm>( who.getID() );
-            sounds::sound( who.pos_bub(), 60, sounds::sound_t::music, _( "an alarm sound!" ), true,
+            sounds::sound( who.pos_bub(), MAX_VIEW_DISTANCE, sounds::sound_t::music, _( "an alarm sound!" ), true,
                            "environment",
                            "alarm" );
             break;