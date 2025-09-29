a35d6d00038053b7d0e46a7e82a7f0df7c12f0b7
Andrew Krieger <akrieger@users.noreply.github.com>
2025-09-29 09:36:17 -0700

    Fix zstd android integration accounting for differences with tlg path variables.

--- android/app/jni/src/third-party/Android.mk
+++ android/app/jni/src/third-party/Android.mk
@@ -12,7 +12,7 @@ LOCAL_CPP_FEATURES := exceptions rtti
 
 # Add your application source files here...
 FLATBUFFERS_SRCS := $(sort $(wildcard $(LOCAL_PATH)/flatbuffers/*.cpp))
-ZSTD_SRCS := $(sort $(wildcard $(LOCAL_PATH)/third-party/zstd/common/*.c) $(wildcard $(LOCAL_PATH)/third-party/zstd/compress/*.c) $(wildcard $(LOCAL_PATH)/third-party/zstd/decompress/*.c))
+ZSTD_SRCS := $(sort $(wildcard $(LOCAL_PATH)/zstd/common/*.c) $(wildcard $(LOCAL_PATH)/zstd/compress/*.c) $(wildcard $(LOCAL_PATH)/zstd/decompress/*.c))
 LOCAL_SRC_FILES := $(sort $(FLATBUFFERS_SRCS:$(LOCAL_PATH)/%=%) $(ZSTD_SRCS:$(LOCAL_PATH)/%=%))
 
 LOCAL_CFLAGS += -DBACKTRACE=1 -DLOCALIZE=1 -DZSTD_STATIC_LINKING_ONLY -DZSTD_DISABLE_ASM -Wextra -Wall -fsigned-char
