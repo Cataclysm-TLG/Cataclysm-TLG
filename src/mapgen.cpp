4931929ae6fbc852d2dc271b3d481a265bdf2160
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2025-09-30 04:33:39 -0700

    Silence two common error messages

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -3431,31 +3431,32 @@ class jmapgen_terrain : public jmapgen_piece_with_has_vehicle_collision
                 dat.m.i_clear( p );
             }
 
-            if( chosen_id != terrain_here ) {
-                std::string error;
-                trap_str_id trap_here = dat.m.tr_at( p ).id;
-                if( act_furn != apply_action::act_ignore && dat.m.furn( p ) != furn_str_id::NULL_ID() ) {
-                    // NOLINTNEXTLINE(cata-translate-string-literal)
-                    error = string_format( "furniture was %s", dat.m.furn( p ).id().str() );
-                } else if( act_trap != apply_action::act_ignore && !trap_here.is_null() &&
-                           trap_here.id() != terrain_here->trap ) {
-                    // NOLINTNEXTLINE(cata-translate-string-literal)
-                    error = string_format( "trap %s existed", trap_here.str() );
-                } else if( act_item != apply_action::act_ignore && !dat.m.i_at( p ).empty() ) {
-                    // NOLINTNEXTLINE(cata-translate-string-literal)
-                    error = string_format( "item %s existed",
-                                           dat.m.i_at( p ).begin()->typeId().str() );
-                }
-                if( !error.empty() ) {
-                    debugmsg( "In %s on %s, setting terrain to %s (from %s) at %s when %s.  "
-                              "Resolve this either by removing the terrain from this mapgen, "
-                              "adding suitable removal commands to the mapgen, or by adding an "
-                              "appropriate clearing flag to the innermost layered mapgen.  "
-                              "Consult the \"mapgen flags\" section in MAPGEN.md for options.",
-                              context, dat.terrain_type().id().str(), chosen_id.id().str(),
-                              terrain_here.id().str(), p.to_string(), error );
-                }
-            }
+            // TODO: Make this less annoying and restore it?
+            // if( chosen_id != terrain_here ) {
+            //     std::string error;
+            //     trap_str_id trap_here = dat.m.tr_at( p ).id;
+            //     if( act_furn != apply_action::act_ignore && dat.m.furn( p ) != furn_str_id::NULL_ID() ) {
+            //         // NOLINTNEXTLINE(cata-translate-string-literal)
+            //         error = string_format( "furniture was %s", dat.m.furn( p ).id().str() );
+            //     } else if( act_trap != apply_action::act_ignore && !trap_here.is_null() &&
+            //                trap_here.id() != terrain_here->trap ) {
+            //         // NOLINTNEXTLINE(cata-translate-string-literal)
+            //         error = string_format( "trap %s existed", trap_here.str() );
+            //     } else if( act_item != apply_action::act_ignore && !dat.m.i_at( p ).empty() ) {
+            //         // NOLINTNEXTLINE(cata-translate-string-literal)
+            //         error = string_format( "item %s existed",
+            //                                dat.m.i_at( p ).begin()->typeId().str() );
+            //     }
+            //     if( !error.empty() ) {
+            //         debugmsg( "In %s on %s, setting terrain to %s (from %s) at %s when %s.  "
+            //                   "Resolve this either by removing the terrain from this mapgen, "
+            //                   "adding suitable removal commands to the mapgen, or by adding an "
+            //                   "appropriate clearing flag to the innermost layered mapgen.  "
+            //                   "Consult the \"mapgen flags\" section in MAPGEN.md for options.",
+            //                   context, dat.terrain_type().id().str(), chosen_id.id().str(),
+            //                   terrain_here.id().str(), p.to_string(), error );
+            //     }
+            // }
             dat.m.ter_set( p, chosen_id );
         }
 
