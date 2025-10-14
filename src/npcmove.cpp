6664c70199d015fcfa0d021ef388f43487817781
Kevin Granade <kevin.granade@gmail.com>
2025-10-13 17:59:03 -0700

    Cap inflation of weapon value based on enemy risk evaluation at 2.0

--- src/npcmove.cpp
+++ src/npcmove.cpp
@@ -4124,7 +4124,7 @@ item *npc::evaluate_best_weapon() const
     double best_value = evaluate_weapon( weap, can_use_gun, use_silent );
 
     // To prevent changing to barely better stuff
-    best_value *= std::max<float>( 1.0f, ai_cache.danger_assessment / 10.0f );
+    best_value *= std::max<float>( 1.0f, std::min<float>( 2.0, ai_cache.danger_assessment / 10.0f ) );
 
     // Fists aren't checked below
     double fist_value = evaluate_weapon( null_item_reference(), can_use_gun, use_silent );
