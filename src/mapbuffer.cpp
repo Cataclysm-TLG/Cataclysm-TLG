CleverRaven/Cataclysm-DDA

initial barebones implementation of world swapping

src/mapbuffer.cpp
@@ -37,12 +37,15 @@ static cata_path find_quad_path( const cata_path &dirname, const tripoint_abs_om
     return dirname / string_format( "%d.%d.%d.map", om_addr.x(), om_addr.y(), om_addr.z() );
 }
 
-static cata_path find_dirname( const tripoint_abs_omt &om_addr )
-{
+static cata_path find_dirname( const tripoint_abs_omt &om_addr, std::string prefix ){
     const tripoint_abs_seg segment_addr = project_to<coords::seg>( om_addr );
-    return PATH_INFO::world_base_save_path_path() / "maps" / string_format( "%d.%d.%d",
-            segment_addr.x(),
-            segment_addr.y(), segment_addr.z() );
+   std::string segment = string_format( "%d.%d.%d",
+                                         segment_addr.x(),
+                                         segment_addr.y(), segment_addr.z() );
+    if( prefix.empty() ) {
+        return PATH_INFO::world_base_save_path_path() / "maps" / segment;
+    }
+    return PATH_INFO::world_base_save_path_path() / "maps" / prefix / segment;
 }
 
 mapbuffer MAPBUFFER;
@@ -134,10 +137,21 @@ bool mapbuffer::submap_exists( const tripoint_abs_sm &p )
     return true;
 }
 
+void mapbuffer::set_prefix( std::string prefix )
+{
+    area_prefix = prefix;
+}
+std::string mapbuffer::get_prefix()
+{
+    return area_prefix;
+}
 void mapbuffer::save( bool delete_after_save )
 {
-    assure_dir_exist( PATH_INFO::world_base_save_path() + "/maps" );
-
+    if( area_prefix.empty() ) {
+        assure_dir_exist( PATH_INFO::world_base_save_path() + "/maps" );
+    } else {
+        assure_dir_exist( PATH_INFO::world_base_save_path() + "/maps/" + area_prefix );
+    }
     int num_saved_submaps = 0;
     int num_total_submaps = submaps.size();
 
@@ -175,7 +189,7 @@ void mapbuffer::save( bool delete_after_save )
         // A segment is a chunk of 32x32 submap quads.
         // We're breaking them into subdirectories so there aren't too many files per directory.
         // Might want to make a set for this one too so it's only checked once per save().
-        const cata_path dirname = find_dirname( om_addr );
+        const cata_path dirname = find_dirname( om_addr, area_prefix );
         const cata_path quad_path = find_quad_path( dirname, om_addr );
 
         bool inside_reality_bubble = here.inbounds( om_addr );
@@ -285,7 +299,7 @@ submap *mapbuffer::unserialize_submaps( const tripoint_abs_sm &p )
 {
     // Map the tripoint to the submap quad that stores it.
     const tripoint_abs_omt om_addr = project_to<coords::omt>( p );
-    const cata_path dirname = find_dirname( om_addr );
+    const cata_path dirname = find_dirname( om_addr, area_prefix );
     cata_path quad_path = find_quad_path( dirname, om_addr );
 
     if( !file_exist( quad_path ) ) {

Cataclysm-TLG/Cataclysm-TLG

src/mapbuffer.cpp
@@ -47,12 +47,16 @@ static std::string quad_file_name( const tripoint_abs_omt &om_addr )
     return string_format( "%d.%d.%d.map", om_addr.x(), om_addr.y(), om_addr.z() );
 }
 
-static cata_path find_dirname( const tripoint_abs_omt &om_addr )
+static cata_path find_dirname( const tripoint_abs_omt &om_addr, std::string prefix )
 {
     const tripoint_abs_seg segment_addr = project_to<coords::seg>( om_addr );
-    return PATH_INFO::world_base_save_path() / "maps" / string_format( "%d.%d.%d",
-            segment_addr.x(),
-            segment_addr.y(), segment_addr.z() );
+    std::string segment = string_format( "%d.%d.%d",
+                                         segment_addr.x(),
+                                         segment_addr.y(), segment_addr.z() );
+    if( prefix.empty() ) {
+        return PATH_INFO::world_base_save_path() / "maps" / segment;
+    }
+    return PATH_INFO::world_base_save_path() / "maps" / prefix / segment;
 }
 
 mapbuffer MAPBUFFER;
@@ -175,10 +179,21 @@ bool mapbuffer::submap_exists_approx( const tripoint_abs_sm &p )
     return true;
 }
 
+void mapbuffer::set_prefix( std::string prefix )
+{
+    area_prefix = prefix;
+}
+std::string mapbuffer::get_prefix()
+{
+    return area_prefix;
+}
 void mapbuffer::save( bool delete_after_save )
 {
-    assure_dir_exist( PATH_INFO::world_base_save_path() / "maps" );
-
+    if( area_prefix.empty() ) {
+        assure_dir_exist( PATH_INFO::world_base_save_path() / "maps" );
+    } else {
+        assure_dir_exist( PATH_INFO::world_base_save_path() / "maps" / area_prefix );
+    }
     int num_saved_submaps = 0;
     int num_total_submaps = submaps.size();
 