53efe790c0048bd21ea38f03851cba6d5fcce159
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2026-02-08 00:18:57 -0800

    Revert "don't copy compression dictionary files to each dimension, as it's appearently not needed"
    This reverts commit ff9aeabb7792178fd9a877084938918d07c15769.

--- src/worldfactory.h
+++ src/worldfactory.h
@@ -90,6 +90,7 @@ struct WORLD {
 
         bool has_compression_enabled() const;
         bool set_compression_enabled( bool enabled ) const;
+        void assure_compression_files_present() const;
 
 };
 
