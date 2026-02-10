CleverRaven/Cataclysm-DDA

weather_generator generic factory support (#82788)
* weather generator generic factory support

* NoHope temporary weather data

to be removed later

src/regional_settings.cpp
@@ -703,7 +703,7 @@ void load_region_settings( const JsonObject &jo )
         jo.throw_error( "\"weather\": { … } required for default" );
     } else {
         JsonObject wjo = jo.get_object( "weather" );
-        new_region.weather.load( wjo, false );
+        new_region.weather.load( wjo, "dda" );
     }
 
     load_overmap_feature_flag_settings( jo, new_region.overmap_feature_flag, strict, false );
@@ -862,7 +862,7 @@ void apply_region_overlay( const JsonObject &jo, regional_settings &region )
 
     if( jo.has_object( "weather" ) ) {
         JsonObject wjo = jo.get_object( "weather" );
-        region.weather.load( wjo, true );
+        region.weather.load( wjo, "dda" );
     }
 
     load_overmap_feature_flag_settings( jo, region.overmap_feature_flag, false, true );

Cataclysm-TLG/Cataclysm-TLG

src/regional_settings.cpp
@@ -698,7 +698,7 @@ void load_region_settings( const JsonObject &jo )
         jo.throw_error( "\"weather\": { … } required for default" );
     } else {
         JsonObject wjo = jo.get_object( "weather" );
-        new_region.weather = weather_generator::load( wjo );
+        new_region.weather.load( wjo, "tlg" );
     }
 
     load_overmap_feature_flag_settings( jo, new_region.overmap_feature_flag, strict, false );
@@ -858,7 +858,7 @@ void apply_region_overlay( const JsonObject &jo, regional_settings &region )
     // TODO: Support overwriting only some values
     if( jo.has_object( "weather" ) ) {
         JsonObject wjo = jo.get_object( "weather" );
-        region.weather = weather_generator::load( wjo );
+        region.weather.load( wjo, "tlg" );
     }
 
     load_overmap_feature_flag_settings( jo, region.overmap_feature_flag, false, true );