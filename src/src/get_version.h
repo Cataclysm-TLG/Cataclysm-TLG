905a20521d32c65ea785e1582009ced342ccc5f1
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2026-04-20 04:10:34 -0700

    wait

--- src/src/get_version.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef CATA_SRC_GET_VERSION
-#define CATA_SRC_GET_VERSION
-const char *getVersionString();
-#endif // CATA_SRC_GET_VERSION
--- src/src/version.h
+++
@@ -1,2 +0,0 @@
-// NOLINT(cata-header-guard)
-#define VERSION "0.G-17257-g45060ea039-dirty"
