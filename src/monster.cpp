f2ee8c59c58ddcae2434d7a6201d1c92077ca445
fairyarmadillo <94415528+worm-girl@users.noreply.github.com>
2026-07-07 15:31:42 -0700

    Sound fixes

--- src/monster.cpp
+++ src/monster.cpp
@@ -4095,9 +4095,10 @@ void monster::hear_sound( const tripoint_bub_ms &source, const int vol, const in
     }
     // Only trigger this if the monster is not friendly or the source isn't us or the player.
     // Skip if the sound is quiet-ish, or we've already hear a louder sound recently.
-    if( source != pos_bub() && volume > 9 && ( friendly == 0 || source != get_player_character().pos_bub() ) && get_effect_int( effect_sound_triggered ) < volume ) {
+    if( source != pos_bub() && volume > 15 && ( friendly == 0 || source != get_player_character().pos_bub() ) && get_effect_int( effect_sound_triggered ) < volume ) {
         add_effect( effect_sound_triggered, ( 1_seconds * rng( 2, 10 ) ), false, std::min( volume, 400 ) );
-        process_trigger( mon_trigger::SOUND, volume );
+        int trigger_strength = std::max( 0, volume - 15 );
+        process_trigger( mon_trigger::SOUND, trigger_strength );
     }
     provocative_sound = tmp_provocative;
     if( morale >= 0 && anger >= 10 ) {
